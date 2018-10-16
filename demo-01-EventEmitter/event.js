0b2833e9b88dd5e137e5077c756880bcbc822f18
v_ljwen <v_ljwen@tencent.com>
2018-10-16 20:05:56 +0800

    EventEmitter

---
+++ demo-01-EventEmitter/event.js
@@ -0,0 +1,11 @@
+// EventEmitter用法
+
+var EventEmitter = require('events').EventEmitter; 
+var emitter = new EventEmitter(); 
+emitter.on('someEvent', function(arg1, arg2) { 
+    console.log('listener1', arg1, arg2); 
+}); 
+emitter.on('someEvent', function(arg1, arg2) { 
+    console.log('listener2', arg1, arg2); 
+}); 
+emitter.emit('someEvent', 'arg1 参数', 'arg2 参数'); 
